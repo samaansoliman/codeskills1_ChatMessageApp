48ae35480bd3ac376208fd9dceef89efd77e9ed7
samaan soliman <71563475+samaansoliman@users.noreply.github.com>
2023-11-06 21:16:57 +0200

    create models and tables for Messages and Conversations & create relationships in the models both

---
+++ app/Models/Message.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Conversation;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+
+        'sender_id',
+        'receiver_id',
+        'conversation_id',
+        'read',
+        'body',
+        'type',
+
+    ];
+
+    // relationship
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+}
